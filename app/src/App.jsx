3d1250cc70d62325e879cd6198241230a8a182db
Paul Thomas <paulthomas2107@gmail.com>
2023-01-25 13:42:15 +0000

    More

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import Editor from '@monaco-editor/react';
 import './App.css';
@@ -17,8 +17,16 @@ const files = {
 };
 function App() {
   const [fileName, setFileName] = useState('script.py');
+  const editorRef = useRef(null);
   const file = files[fileName];
 
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function getEditorValue() {
+    alert(editorRef.current.getValue());
+  }
   return (
     <div className="App">
       <button onClick={() => setFileName('index.html')}>
@@ -27,10 +35,12 @@ function App() {
       <button onClick={() => setFileName('script.py')}>
         Switch to script.py
       </button>
+      <button onClick={() => getEditorValue()}>Get Editor Value</button>
       <Editor
         height="100vh"
         width="100%"
         theme="vs-dark"
+        onMount={handleEditorDidMount}
         path={file.name}
         defaultLanguage={file.language}
         defaultValue={file.value}
